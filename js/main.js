82208c702923903d90dc02724dfc9ecb0164c811
pablo.cristini <pablitoocristini@gmail.com>
2022-08-16 16:40:53 -0300

    Agregando librerias

--- js/main.js
+++ js/main.js
@@ -22,11 +22,41 @@ productos.forEach((producto) => {
         const total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
         document.getElementById("cart-total").innerHTML = `${carrito.length}  $${total}`;
         localStorage.setItem("carrito", JSON.stringify(carrito));
+        Toastify({
+            text:"Producto agregado",
+            duration: 1500,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "bottom",
+            position: "right",
+            stopOnFocus: false,
+            style: {background: "#1fff39"},
+            onClick: function(){}
+        }).showToast();
     });
 });
 
 const vaciarCarrito = document.getElementById('vaciar-carrito');
 vaciarCarrito.addEventListener('click',() => {
-    carrito = [];
     document.getElementById("cart-total").innerHTML = `${carrito.length}  $${total}`;
+
+    Swal.fire({
+        title: 'Desea vaciar el carrito?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, vaciar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            carrito = [];
+        Swal.fire(
+            'Vacio',
+            'Se han borrado los productos del carrito',
+            'success'
+        )} else if (result.isDenied) {
+            carrito = carrito;
+        }
+    })
 });
